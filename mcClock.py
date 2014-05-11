d70f4659e0d011ec471907e3c5db4fbeda9c6f4d
Lorenzo Flueckiger <lorenzo@flupes.org>
2014-05-11 00:06:03 -0700

    Started to use the VLC player
    Things seems to work mostly, but the logic for multiple buttons press is
    broken, thus brightness cannot be controlled.

--- mcClock.py
+++ mcClock.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python
 
+# Hardcode some module path to simplify
+import sys
+basedir='/home/pi/code'
+sys.path.append(basedir+'/Adafruit-Raspberry-Pi-Python-Code/Adafruit_LEDBackpack')
+sys.path.append(basedir+'/pyvlc')
+
+import vlc
 import RPi.GPIO as GPIO
 from Adafruit_7Segment import SevenSegment
+
+import glob
 import time
 import datetime
 
@@ -9,8 +18,7 @@ import datetime
 segment = SevenSegment(address=0x70)
 
 # Set the default brightness
-brightness=8
-segment.disp.setBrightness(brightness)
+segment.disp.setBrightness(6)
 
 # Use BMC numbering
 GPIO.setmode(GPIO.BCM)
@@ -24,25 +32,108 @@ buttons = {
     27 : "DOWN",
 }
 
+# Configure a VLC player
+player = vlc.MediaPlayer()
+player.audio_set_volume(60)
+
+# List of songs
+musicdir = '/home/pi/Music/Minecraft'
+songs = glob.glob(musicdir+'/*.mp3')
+medialist = vlc.MediaList(songs)
+
+# MediaListPlayer
+mlplayer = vlc.MediaListPlayer()
+mlplayer.set_media_player(player)
+mlplayer.set_media_list(medialist)
+
+pressed = { }
+set_button = None
+set_time = None
+
+# Detect two buttons pressed simultaneously
+def check_set(pin, ts):
+    # This does NOT work! (or rather the use of this function is wrong)
+    if ( set_button != None ) :
+        if ( GPIO.input(pin) == False ) :
+            if ( (time.time()-ts) > 0.4 ) :
+                return True
+    return False
+
 # Callback when tactile switch is released
 def button_callback(channel):
-    print("Button "+buttons[channel]+"("+str(channel)+") RELEASED")
-    global brightness
-    if ( buttons[channel] == "UP" ) :
-        if ( brightness < 15 ) :
-            brightness = brightness + 1
-            segment.disp.setBrightness(brightness)
+
+    # global pressed
+    # elapsed = 0.
+    # longpress = None
+    # if (GPIO.input(channel) == False):
+    #     pressed[channel] = time.time()
+    #     print "button pressed"
+    # else:
+    #     if ( pressed.has_key(channel) ):
+    #         ts = time.time()
+    #         elapsed = ts-pressed[channel]        
+    #         print "elapsed =", elapsed
+    #     else:
+    #         print "no timestamp for pressed!"
+    #     if ( elapsed > 0.4 ):
+    #         longpress = True
+    #     else:
+    #         longpress = False
+
+#    if (longpress): p='LONG'
+#    else: p='SHORT'
+#    print("Button "+buttons[channel]+"("+str(channel)+") "+p+" pressed")
+
+    global set_button, set_time
+
+    if ( buttons[channel] == "SET" ) :
+        set_button = channel
+        set_time = time.time()
+        if ( player.is_playing() ) :
+            mlplayer.pause()
+        else :
+            mlplayer.play()
+            print(player.get_media().get_mrl())
+    elif ( buttons[channel] == "UP" ) :
+        if ( check_set(set_button, set_time) ) :
+            brightness = segment.disp.getBrightness()
+            if ( brightness < 15 ) :
+                brightness = brightness + 1
+                segment.disp.setBrightness(brightness)
+            print("brightness="+str(brightness))
+        else :
+            volume = player.audio_get_volume()
+            if ( volume < 91 ) :
+                volume = volume + 10
+                player.audio_set_volume(volume)
+                print("volume="+str(volume))
     elif ( buttons[channel] == "DOWN" ) :
-        if ( brightness > 0 ) :
-            brightness = brightness - 1
-            segment.disp.setBrightness(brightness)
-    print("brightness="+str(brightness))
+        if ( check_set(set_button, set_time) ) :
+            brightness = segment.disp.getBrightness()
+            if ( brightness > 0 ) :
+                brightness = brightness - 1
+                segment.disp.setBrightness(brightness)
+            print("brightness="+str(brightness))
+        else :
+            volume = player.audio_get_volume()
+            if ( volume > 9 ) :
+                volume = volume - 10
+                player.audio_set_volume(volume)
+                print("volume="+str(volume))
+    elif ( buttons[channel] == "LEFT" ) :
+        mlplayer.previous()
+        print(player.get_media().get_mrl())
+    elif ( buttons[channel] == "RIGHT" ) :
+        mlplayer.next()
+        print(player.get_media().get_mrl())
+
+    # print("brightness="+str(brightness))
 
 # Initialize the tactile switches inputs and callback
 for pin in buttons:
     print("configure button "+buttons[pin]+" on pin "+str(pin))
     GPIO.setup(pin, GPIO.IN)
-    GPIO.add_event_detect(pin, GPIO.RISING, callback=button_callback, bouncetime=250)
+    GPIO.add_event_detect(pin, GPIO.FALLING, callback=button_callback, bouncetime=250)
     
 def update_clock():
     now = datetime.datetime.now()
