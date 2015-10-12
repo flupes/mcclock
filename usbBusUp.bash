2f490ea272e893db779678fc5d5742e464a0cab6
Lorenzo Flueckiger <lorenzo@flupes.org>
2015-10-12 11:02:24 -0700

    turn usb on already if previously off

--- usbBusUp.bash
+++ usbBusUp.bash
@@ -2,10 +2,15 @@
 
 usbDevice=/sys/devices/platform/soc/20980000.usb/buspower
 
-echo "powering up usb bus..."
-echo "0x1" > $usbDevice
+state=`cut -d " " -f 4 $usbDevice`
 
-sleep 1
+if [ $state == "0x1" ] ; then
+    echo "usb bus is already up!"
+else
+    echo "powering up usb bus..."
+    echo "0x1" > $usbDevice
+    sleep 5
+fi
 
 echo "starting network services..."
 /etc/init.d/networking start
