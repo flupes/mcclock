1e610f4369595067dca475fb8e399a2adb2ca551
Lorenzo Flueckiger <lorenzo@flupes.org>
2016-01-24 10:52:08 -0800

    added a connectivity test after starting the network

--- usbBusUp.bash
+++ usbBusUp.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 usbDevice=/sys/devices/platform/soc/20980000.usb/buspower
+check_host="www.pandora.com"
 
 state=`cut -d " " -f 4 $usbDevice`
 
@@ -9,8 +10,21 @@ if [ $state == "0x1" ] ; then
 else
     echo "powering up usb bus..."
     echo "0x1" > $usbDevice
-    sleep 5
+    sleep 1
 fi
 
 echo "starting network services..."
 /etc/init.d/networking start
+
+for i in `seq 1 10` ; do
+    sleep 1
+    ping -c 1 $check_host > /dev/null
+    ret=$?
+    if [ $ret -eq 0 ] ; then
+	echo "ping $check_host successfull"
+	exit 0
+    fi
+done
+echo "failed to ping $check_host"
+exit 1
+
