89dbb191fbca1eec8ae792bda61020ff42dd9a1c
Lorenzo Flueckiger <lorenzo@flupes.org>
2016-01-24 19:20:57 -0800

    added temporary message capability

--- char_display.py
+++ char_display.py
@@ -3,25 +3,30 @@ import time
 import Adafruit_CharLCD as LCD
 
 class CharDisplay(object):
+    # Note: line and pos arguments are given from 1 to number_of_lines
+    # end internally converted to 0..number_of_lines
 
     NUMBER_LINES = 2
     NUMBER_CHARS = 16
     
-    lines = list(xrange(NUMBER_LINES))
-    periods = list(xrange(NUMBER_LINES))
-    timestamps = list(xrange(NUMBER_LINES))
-    positions = list(xrange(NUMBER_LINES))
-    indices = list(xrange(NUMBER_LINES))
+    lines = ["" for x in range(NUMBER_LINES)]
+    tempmsgs = ["tmp" for x in range(NUMBER_LINES)]
+    periods = [0 for x in range(NUMBER_LINES)]
+    timestamps = [0 for x in range(NUMBER_LINES)]
+    expirations = [0 for x in range(NUMBER_LINES)]
+    positions = [0 for x in range(NUMBER_LINES)]
+    indices = [0 for x in range(NUMBER_LINES)]
     #indices = [[0 for x in range(2)] for y in range(NUMBER_LINES)]
         
     def __init__(self):
         self.lcd = LCD.Adafruit_CharLCDPlate()
         self.lcd.clear()
-        self.static_msg(0, "Hello", 5)
-        self.static_msg(1, "World", 5)
+        self.static_msg(1, "Hello", 5)
+        self.static_msg(2, "World", 5)
 
     def scroll_msg(self, line, msg, period):
-        if line < self.NUMBER_LINES:
+        if (line>0) and (line<=self.NUMBER_LINES):
+            line = line - 1
             self.lines[line] = msg + '   '
             self.periods[line] = period
             self.timestamps[line] = time.time() + period*(self.NUMBER_CHARS/2)
@@ -33,21 +38,29 @@ class CharDisplay(object):
             self.__draw__(line)
         
     def static_msg(self, line, msg, pos):
-        if line < self.NUMBER_LINES:
+        if (line>0) and (line<=self.NUMBER_LINES):
+            line = line -1
+            pos = pos - 1
             self.lines[line] = msg
             self.periods[line] = 0
             self.positions[line] = pos
-            sz = len(msg)+pos
-            if sz > self.NUMBER_CHARS:
-                self.indices[line] = self.NUMBER_CHARS+1
-            else:
-                self.indices[line] = sz+1
             self.__draw__(line)
 
+    def timed_msg(self, line, msg, duration):
+        if (line>0) and (line<=self.NUMBER_LINES):
+            line = line - 1
+            msg = msg + "                        "
+            self.tempmsgs[line] = msg[0:self.NUMBER_CHARS]            
+            self.expirations[line] = time.time()+duration
+            self.__draw__(line)
+            
     def __draw__(self, l):
-        if self.periods[l] == 0:
+        if self.expirations[l] > 0:
+            self.lcd.set_cursor(0, l)
+            self.lcd.message(self.tempmsgs[l])
+        elif self.periods[l] == 0:
             self.lcd.set_cursor(self.positions[l], l)
-            self.lcd.message((self.lines[l])[0:self.indices[l]])
+            self.lcd.message(self.lines[l])
         else:
             self.lcd.set_cursor(0, l)
             s = len(self.lines[l])
@@ -62,6 +75,10 @@ class CharDisplay(object):
         # tick the display to allow dynamic effects
         now = time.time()
         for l in range(0, self.NUMBER_LINES):
+            if self.expirations[l] > 0:
+                if now > (self.expirations[l]):
+                    self.expirations[l] = 0
+                    self.__draw__(l)
             if self.periods[l] > 0:
                 if now > (self.timestamps[l]):
                     self.timestamps[l] = now+self.periods[l]
--- test_CharDisplay.py
+++ test_CharDisplay.py
@@ -6,12 +6,17 @@ disp = CharDisplay()
 
 time.sleep(3)
 
-disp.static_msg(0, "This is a static line", 0)
-#disp.static_msg(1, "And a second.", 2)
-disp.scroll_msg(1, "[And a long second line that scrolls]", 0.2)
+disp.static_msg(1, "Text on first line", 1)
+disp.scroll_msg(2, "[And a long second line that scrolls]", 0.2)
 
-for i in range(0,300):
+for i in range(0,400):
     disp.update()
+    if i == 60:
+        disp.timed_msg(1, "Temporary message", 8)
+    if i == 100:
+        disp.timed_msg(2, "  it will last 6s", 6)
+    if i == 200:
+        disp.static_msg(2, "New text on 2nd!", 1)
     time.sleep(0.1)
 
 disp.lcd.clear()
